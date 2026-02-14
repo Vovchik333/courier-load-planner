da00fa8ba4663e8c3764d14d000aac20dbedf4f3
Volodymyr <vavan123music@gmail.com>
2026-02-14 20:58:34 +0200

    added react and tailwind

---
+++ client/src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+export const Header: React.FC = () => {
+  return (
+    <header className="flex w-full justify-between px-4 py-2">
+      <h1>
+        Courier Load Planner - Day View
+      </h1>
+      <p>
+        Date:
+      </p>
+      <input type="text" />
+    </header>
+  );
+}
